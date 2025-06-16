1b1de4fc0a3f6503d665233f31501645cacb56fe
Julio Paim <julio.paim@klassmatt.com>
2025-06-16 10:14:37 -0300

    ajuste no banco de dados

--- lib/database.dart
+++ lib/database.dart
@@ -15,42 +15,43 @@ void iniciaBanco() {
     DROP TABLE Eventos;
     DROP TABLE Usuarios;
 
-    CREATE TABLE IF NOT EXISTS Entidades (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    sigla TEXT NOT NULL,
-    nome TEXT NOT NULL,
-    fundado DATE NOT NULL,
-    rt INTEGER NOT NULL,
-    cidade TEXT NOT NULL,
-    endereco TEXT,
-    verificado BOOLEAN NOT NULL DEFAULT 0,
-    UNIQUE (sigla, nome, rt)
-    );
-
-    CREATE TABLE IF NOT EXISTS Eventos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    organizador TEXT NOT NULL,
-    dataRealizacao DATE NOT NULL,
-    tipoEvento TEXT NOT NULL,
-    dataInscricao DATE,
-    cidade TEXT NOT NULL,
-    endereco TEXT NOT NULL,
-    premio TEXT,
-    contato TEXT,
-    verificado BOOLEAN NOT NULL DEFAULT 0,
-    UNIQUE (tipoEvento, dataRealizacao, organizador, endereco)
-    );
-
-    CREATE TABLE IF NOT EXISTS Usuarios(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    login TEXT NOT NULL UNIQUE,
-    email TEXT,
-    senha TEXT NOT NULL,
-    admin BOOLEAN NOT NULL DEFAULT 0
-    );
+    
     
   ''');
 
 }
 
+// CREATE TABLE IF NOT EXISTS Entidades (
+// id INTEGER PRIMARY KEY AUTOINCREMENT,
+// sigla TEXT NOT NULL,
+// nome TEXT NOT NULL,
+// fundado DATE NOT NULL,
+// rt INTEGER NOT NULL,
+// cidade TEXT NOT NULL,
+// endereco TEXT,
+// verificado BOOLEAN NOT NULL DEFAULT 0,
+// UNIQUE (sigla, nome, rt)
+// );
+//
+// CREATE TABLE IF NOT EXISTS Eventos (
+// id INTEGER PRIMARY KEY AUTOINCREMENT,
+// organizador TEXT NOT NULL,
+// dataRealizacao DATE NOT NULL,
+// tipoEvento TEXT NOT NULL,
+// dataInscricao DATE,
+// cidade TEXT NOT NULL,
+// endereco TEXT NOT NULL,
+// premio TEXT,
+// contato TEXT,
+// verificado BOOLEAN NOT NULL DEFAULT 0,
+// UNIQUE (tipoEvento, dataRealizacao, organizador, endereco)
+// );
+//
+// CREATE TABLE IF NOT EXISTS Usuarios(
+// id INTEGER PRIMARY KEY AUTOINCREMENT,
+// nome TEXT NOT NULL,
+// login TEXT NOT NULL UNIQUE,
+// email TEXT,
+// senha TEXT NOT NULL,
+// admin BOOLEAN NOT NULL DEFAULT 0
+// );
